6ab5350fb00e2e6e710082142a57617307670725
phantware <phantmoney2011@gmail.com>
2020-05-02 12:57:45 +0100

    post router and queries created

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const monsters = require("./router/monster");
 const app = express();
 
+app.use(express.json());
 app.use("/monsters", monsters);
 
 app.use((err, req, res, next) => {
--- router/monster.js
+++ router/monster.js
@@ -5,7 +5,7 @@ const router = Router();
 router.get("/", (req, res, next) => {
   pool.query("SELECT * FROM monsters ORDER BY id ASC", (err, resp) => {
     if (err) return next(err);
-    res.json(resp.rows);
+    return res.json(resp.rows);
   });
 });
 
@@ -13,8 +13,20 @@ router.get("/:id", (req, res, next) => {
   const { id } = req.params;
   pool.query("SELECT * FROM monsters WHERE id = $1", [id], (err, resp) => {
     if (err) return next(err);
-    res.json(resp.rows);
+    return res.json(resp.rows);
   });
+});
+
+router.post("/", (req, res, next) => {
+  const { name, personality } = req.body;
+  pool.query(
+    "INSERT INTO monsters (name, personality) VALUES ($1,$2)",
+    [name, personality],
+    (err, resp) => {
+      if (err) return next(err);
+      return res.redirect("/monsters");
+    }
+  );
 });
 
 module.exports = router;
